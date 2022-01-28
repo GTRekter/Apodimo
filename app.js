47ee816440a17fa89c07aef015736008b22efd31
Ivan Porta <ivanporta@microsoft.com>
2022-01-28 16:09:51 +0100

    creation issuea from azure devops

--- app.js
+++ app.js
@@ -67,6 +67,26 @@ function migrateWorkItems() {
                     if(workItemIds.length > 0) {
                         const workItems = await azureDevOpsService.getWorkItems(workItemIds);
                         console.log(workItems);
+                        workItems.map(async workItem => {
+                            const lables = [
+                                workItem.fields["System.WorkItemType"],
+                                workItem.fields["System.AreaPath"]
+                            ]
+                            const body = `<h1>${workItem.fields["System.Title"]}</h1>
+                                <p>Priority: ${workItem.fields["Microsoft.VSTS.Common.Priority"]}</p>
+                                <p>Effort: ${workItem.fields["Microsoft.VSTS.Common.Effort"]}</p>
+                                <p>RemainingWork: ${workItem.fields["Microsoft.VSTS.Common.RemainingWork"]}</p>
+                                <p>IterationPath: ${workItem.fields["System.IterationPath"]}</p>
+                                <p>Reason: ${workItem.fields["System.Reason"]}</p>
+                                <p>State: ${workItem.fields["System.State"]}</p>
+                                <p>CreatedDate: ${workItem.fields["System.CreatedDate"]}</p>
+                                <p>CreatedBy: ${workItem.fields["System.CreatedBy"]}</p>
+                                <p>ChangedDate: ${workItem.fields["System.ChangedDate"]}</p>
+                                <p>ChangedBy: ${workItem.fields["System.ChangedBy"]}</p>
+                                <p>Description: ${workItem.fields["System.Description"]}</p>`;
+                            const gitHubIssue = await gitHubService.createIssue("GTRekter", gitHubProject, workItem.fields["System.Title"], body, lables);
+                            console.log(gitHubIssue);
+                        });
                     }
                 });
 
--- services/githubService.js
+++ services/githubService.js
@@ -18,7 +18,6 @@ class GitHubService {
     async getUser() {
         return await this.octokit.users.getAuthenticated();
     }
-
     async createPage(owner, repo, branch, path = "/") {
         const { data } = await this.octokit.rest.repos.createPagesSite({
             owner: owner,
@@ -30,9 +29,26 @@ class GitHubService {
         })
         return data;
     }
-
-
-
-
+    async createIssue(owner, repo, title, body, labels = []) {
+        const { data } = await this.octokit.rest.issues.create({
+            owner: owner,
+            repo: repo,
+            title: title,
+            body: body,
+            labels: labels
+        })
+        return data;
+    }
+    async createMilestone(owner, repo, title, state, description, due_on) {
+        const { data } = await this.octokit.rest.issues.createMilestone({
+            owner: owner,
+            repo: repo,
+            title: title,
+            state: state,
+            description: description,
+            due_on: due_on
+        });
+        return data;
+    }
 }
 module.exports = GitHubService;
